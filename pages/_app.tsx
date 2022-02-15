078666441a2c028b08d2547ff3da20d70be54d90
- <->
2022-02-14 22:52:00 -0700

    Initial commit with something rendering without `window is undefined` error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,31 @@
 import '../styles/globals.css'
+import "../styles/choose-token.scss";
+import "../styles/bond.scss";
+import "../styles/bondSettings.scss";
+import "../styles/zapin.scss";
+import "../styles/drawer-content.scss";
+import "../styles/rebasetimer.scss";
+import "../styles/wrap.scss";
+import "../styles/loader.scss";
+import "../styles/wrap-button.scss";
+import "../styles/time-menu.scss";
+import "../styles/header.scss";
+import "../styles/connect-menu.scss";
+import "../styles/view-base.scss";
+
 import type { AppProps } from 'next/app'
+import { Provider } from "react-redux";
+import { Web3ContextProvider } from "../hooks";
+import store from '../store/store'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Web3ContextProvider>
+        <Component {...pageProps} />
+      </Web3ContextProvider>
+    </Provider>
+  )
 }
 
 export default MyApp
